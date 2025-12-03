bb542f606d9c19d4a458752b6b9336c1ef186629
Shaken Shyngyssuly <shaken2005kz@gmail.com>
2025-12-03 09:44:21 +0500

    create WorkoutRepository

--- app/src/main/java/com/example/fitnessplusapp/data/repository/WorkoutRepository.kt
+++ app/src/main/java/com/example/fitnessplusapp/data/repository/WorkoutRepository.kt
@@ -1,4 +1,22 @@
 package com.example.fitnessplusapp.data.repository
 
-class WorkoutRepository {
+import androidx.lifecycle.LiveData
+import com.yourpackage.fitnessplus.data.local.dao.WorkoutDao
+import com.yourpackage.fitnessplus.data.local.entity.WorkoutEntity
+
+class WorkoutRepository(private val workoutDao: WorkoutDao) {
+
+    val allWorkouts: LiveData<List<WorkoutEntity>> = workoutDao.getAllWorkouts()
+
+    suspend fun insert(workout: WorkoutEntity) {
+        workoutDao.insertWorkout(workout)
+    }
+
+    suspend fun delete(workout: WorkoutEntity) {
+        workoutDao.deleteWorkout(workout)
+    }
+
+    fun getWorkoutsByCategory(category: String): LiveData<List<WorkoutEntity>> {
+        return workoutDao.getWorkoutsByCategory(category)
+    }
 }
